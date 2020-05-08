e58c513fcfc66509cfb82ac1f5984dd55f981987
Maciek Lubocki <mail2maciek@gmail.com>
2020-05-08 20:05:21 +0200

    Calculator remodelling

--- Calculator.py
+++ Calculator.py
@@ -2,15 +2,19 @@
 # Import modules
 
 import logging
-import string
-import inspect
-import sys
 
 logging.basicConfig(level=logging.DEBUG)
 
+
+# tutaj przeniosłem deklarację function_name mam błąd związany ze zmiennymi calculations/operation...co z tym zrobić?
+function_name = calculations[operation].__name__
+
 # Function definitions
 def calculator(arg_number_1, arg_number_2):
-    print("You have chosen ", function_name, ". So: ", arg_number_1, operation  ,arg_number_2, "=", calculations[operation](arg_number_1, arg_number_2))
+    
+# zmiana z print na loggin plus złamanie linii
+    logging.info("You have chosen ", function_name, ". So: ", arg_number_1, operation  ,arg_number_2, "=", 
+    calculations[operation](arg_number_1, arg_number_2))
    
 def add(a, b):
     return a + b
@@ -25,7 +29,7 @@ def div(a, b):
     return a / b
 
 
-def testify(arg_test):
+def get_argument(arg_test):
     while True:
         try:
             arg_test = int(input(arg_test))
@@ -35,35 +39,35 @@ def testify(arg_test):
         else:   
             logging.info("Arg_test value is: ", arg_test)
             return arg_test
-            break
             
+            
+#przeniosłem do funkcji poniższy fragment, funkcja bez argumentów)
+def calc():
+
+    if __name__ == "__main__":
 
 
-# Executable part of code
-if __name__ == "__main__":
+        calculations = {
+            "+": add,
+            "-": sub,
+            "*": mul,
+            "/": div
+        }
 
+        print('What do you want me to do (choose appropriate number): ')
+        for item in calculations:
+            print(item)
 
-    calculations = {
-        "+": add,
-        "-": sub,
-        "*": mul,
-        "/": div
-    }
-
-    print('What do you want me to do (choose appropriate number): ')
-    for item in calculations:
-        print(item)
-
-
+        operation = input("Enter correct operator:")
+                
+        arg_number_1 = get_argument("Enter first number: ")
+        arg_number_2 = get_argument("Enter second number: ")
     
-    operation = input("Enter correct operator:")
-    function_name = calculations[operation].__name__
+        return arg_number_1, arg_number_2;
     
+    logging.info(calc())
 
-arg_number_1 = testify("Enter first number: ")
-arg_number_2 = testify("Enter second number: ")
-
-
-calculator(arg_number_1, arg_number_2)
+#chciałem wywołać funkcję w funkcji ale komunikuje że nie zna drugiej zmiennej którą przecież zawarłem w funkcji calc() i oddaję w return
+calculator(calc())
 
 
