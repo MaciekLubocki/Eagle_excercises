7614fbbb84f25c3b55fcf0e2dec051c13b963470
Maciek Lubocki <mail2maciek@gmail.com>
2020-05-21 12:32:30 +0200

    Biznes cards code develope

--- biznes_cards.py
+++ biznes_cards.py
@@ -1,54 +1,82 @@
-class Card:
-  def __init__(self, first_name, second_name, phone, position, company, work_phone, email):
+from faker import Faker
+fake = Faker()
+
+
+class BaseContact:
+  def __init__(self, first_name, second_name, phone, email):
        self.first_name = first_name
        self.second_name = second_name
        self.phone = phone
+       self.email = email
+
+       self._contact = f' Wybieram numer {self.phone} i dzwonię do {self.first_name} {self.second_name}'
+       self._label_length= 0
+
+  def __str__(self):
+      return f' {self._contact} / {self.email}, ilość liter (im/naz): {self.label_length}'
+
+  @property
+  def label_length(self):
+     fn_count = len(self.first_name)
+     sn_count = len(self.second_name)
+     return fn_count, sn_count
+
+class BusinessContact(BaseContact):
+  def __init__(self, position, company, work_phone,  *args, **kwargs):
+       super().__init__(*args, **kwargs)
        self.position = position
        self.company = company
        self.work_phone = work_phone
-       self.email = email
-       
-class BaseContact(Card):
-  def __init__(self, first_name, second_name, phone, email):
-       super().__init__(*args, **kwargs)
-       self.max_load = max_load
 
-class BiznesContact(Card):
-  def __init__(self, position, company, work_phone):
-       super().__init__(*args, **kwargs)
-       self.max_load = max_load
-
-    # variables       
-
-       self._contact = "Kontaktuję się z …"
-       self._letters_count= 0
-
-
-  @property
-  def letters_count(self):
-   fn_count = len(self.first_name)
-   sn_count = len(self.second_name)
-   
-   return fn_count, sn_count
+      # variables       
+       self._contact = f' Wybieram numer {self.work_phone} i dzwonię do {self.first_name} {self.second_name}'
+       self._label_length= 0
 
   def __str__(self):
-      return f'{self._contact} {self.first_name} {self.second_name} {self.email} {self.letters_count}'
+      return f'{self._contact} / {self.email} na stanowisku: {self.position} w: {self.company} tel.: {self.work_phone} ilość liter (im/naz): {self.label_length}'
 
-andrzej_witkowski = Card(first_name="Andrzej", second_name="Witkowski", phone="123-454-654", position="police officer", company="Dark sides", work_phone="111-555-888", email="awitkowski@ggmail.pl")
-wojciech_sumliński = Card(first_name="Wojciech", second_name="Sumliński", phone="133-554-634", position="journalist", company="Real Truth", work_phone="222-444-", email="wsumlinski@op.pl")
-paweł_chojecki = Card(first_name="Paweł", second_name="Chojecki", phone="153-464-634", position="office worker", company="Witness of Smith", work_phone="", email="pchojecki@ipex.pl")
-marcin_rola = Card(first_name="Marcin", second_name="Rola", phone="323-424-594", position="office assistant", company="Tower Office", work_phone="", email="mrola@waw.pl")
-stanisław_michalkiewicz = Card(first_name="Stanisław" , second_name="Michalkiewicz", phone="123-454-654", position="author", company="self employed", work_phone="", email="smichalkiewicz@edu.pl")
+  @property
+  def label_length(self):
+   fn_count = len(self.first_name)
+   sn_count = len(self.second_name)
+   return fn_count, sn_count
 
-Card = [andrzej_witkowski, wojciech_sumliński, paweł_chojecki, marcin_rola, stanisław_michalkiewicz]
-by_first_name = sorted(Card, key=lambda Card: Card.first_name)
-by_second_name = sorted(Card, key=lambda Card: Card.second_name)
-by_email = sorted(Card, key=lambda Card: Card.email)
 
-# print('By first name')
-# print(*by_first_name, sep='\n')
-# print('By second name')
-# print(*by_second_name, sep='\n')
-# print('By email')
-print(*by_email, sep='\n')
+def cards_creator(amount, base_amount, business_amount):
+  card_list=''  
+  card_sum = 0
+  lp = 1
+  i = 0
+  amount = str.split(input("Provide splited by comma how much cards do you need to create (1.Base_type, 2.Business_type): "))
+  base_amount= amount[0]
+  business_amount = amount[1]
+  if int(base_amount) != 0 and int(base_amount) > 0:
+    type_card = "BaseCard"
+    for i in range(int(base_amount)):
+      card_list += (f""" {lp} = BaseContact(first_name={fake.first_name()}, second_name={fake.last_name()}, phone={fake.phone_number()}, email={fake.email()} )""") + "\n"
+      lp = lp + 1
+  if int(business_amount) != 0 and int(business_amount) > 0:
+    for i in range(int(business_amount)):
+      card_list +=  (f""" {lp} = BusinessContact(first_name={fake.first_name()}, second_name={fake.last_name()}, position={fake.job()},  company={fake.company()},  work_phone={fake.phone_number()})""") + "\n"
+      lp = lp + 1
+      card_sum = (int(base_amount) + int(business_amount))  
+  print(card_list)
+  
+  cards_creator.contact_list = [(item + 1) for item  in range(card_sum)]
+  print(cards_creator.contact_list)
+  
+cards_creator('','','') 
+
+# base_contact = str(1)
+# for person in cards_creator.contact_list:
+#    if person == cards_creator.card_list:
+#     base_contact += base_contact + type(person)
+
+base_contact = [item for item in cards_creator.contact_list if type(item) == BaseContact]
+business_contact = [item for item in cards_creator.contact_list if type(item) == BusinessContact]
+
+print(*base_contact, sep="\n")
+print('')
+print('')
+print(*business_contact, sep="\n")
 
