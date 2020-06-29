accf352257e6fc01d917d0cb7f6167b37b0b385a
Maciek Lubocki <mail2maciek@gmail.com>
2020-06-29 13:39:22 +0200

    Code upgrade

--- biznes_cards.py
+++ biznes_cards.py
@@ -1,32 +1,38 @@
 # import biblioteki zewnętrznej
 
 from faker import Faker
-fake = Faker()
+fake = Faker("pl_PL")
 
 # deklaracja klasy BaseContact
 
 
 class BaseContact:
-    def __init__(self, first_name, second_name, phone, email):
+    def __init__(self, intro, first_name, second_name, phone, email):
         self.first_name = first_name
         self.second_name = second_name
         self.phone = phone
         self.email = email
-        self.fname_length = len(first_name)
-        self.sname_length = len(second_name)
 
-    @classmethod
-    def generate(cls, number_of_contacts):
-        cards = []
-        for i in range(0, int(number_of_contacts)):
-            first_name = fake.first_name()
-            second_name = fake.last_name()
-            phone = fake.phone_number()
-            email = fake.ascii_company_email()
-            base_card = BaseContact(first_name, second_name, phone, email)
-            cards.append(base_card)
-            print(first_name, second_name, phone, email)
-        return cards
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.second_name}"
+
+    @property
+    def label_length(self):
+        # return len(self.full_name)
+        return len(self.first_name), len(self.second_name)
+
+    @property
+    def contact_number(self):
+        return self.phone
+
+    # @property
+    # def intro(self):
+    #     return "Biznes cards = "
+
+    def contact(self):
+        return f"Wybieram numer {self.contact_number} i dzownię do {self.full_name}"
+
 
 # deklaracja klasy BusinessContact
 
@@ -38,35 +44,46 @@ class BusinessContact(BaseContact):
         self.company = company
         self.work_phone = work_phone
 
+    @property
+    def contact_number(self):
+        return self.work_phone
+
 
 def create_contacts(card_type, amount):
     cards = []
-    card_string = ''
-    lp = 1
-    i = 0
     query = input("Provide splited by comma \n type of cards you need \n (Please choose by number 1. Base Type, 2.Business Type) \n and how many of those do you need : ").split(",")
-    card_type = query[0]
-    amount = query[1]
+    card_type = int(query[0])
+    amount = int(query[1])
     for i in range(int(amount)):
-        first_name = fake.first_name()
-        second_name = fake.last_name()
-        fname_length = len(first_name)
-        sname_length = len(second_name)
-        if int(card_type) == 1:
-            card_string += (f""" {(first_name.lower() + '_' + second_name.lower())} = BaseContact(first_name="{first_name}", second_name="{second_name}", phone="{fake.phone_number()}", email="{fake.email()}", ilość liter (im/naz): "{fname_length}/{sname_length}" )""") + "\n"
-        else:
-            card_string += (f""" {(first_name.lower() + '_' + second_name.lower())} = BusinessContact(first_name="{first_name}", second_name="{second_name}", phone="{fake.phone_number()}", position="{fake.job()}", company="{fake.company()}", work_phone="{fake.phone_number()}", email="{fake.email()}",  ilość liter (im/naz): "{fname_length}/{sname_length}")""") + "\n"
-        lp = lp + 1
-    print(card_string)
-    return card_type, amount
+        if card_type == 1:
+            cards.append(
+                BaseContact(
+                    intro="Business cards = ",
+                    first_name=fake.first_name(),
+                    second_name=fake.last_name(),
+                    phone=fake.phone_number(),
+                    email=fake.email(),
+
+                )
+            )
+        elif card_type == 2:
+            cards.append(
+                BusinessContact(
+                    intro="Base cards = ",
+                    first_name=fake.first_name(),
+                    second_name=fake.last_name(),
+                    phone=fake.phone_number(),
+                    position=fake.job(),
+                    company=fake.company(),
+                    work_phone=fake.phone_number(),
+                    email=fake.email(),
+
+                )
+            )
+    return cards
 
 
 if __name__ == "__main__":
-
-    create_contacts('', '')
-
-    print('')
-    BaseContact.generate(5)
-    print('')
-    print('')
-    BusinessContact.generate(5)
+    cards = create_contacts('', '')
+    for card in cards:
+        print(card.contact(), '. Długość im/naz: ', card.label_length)
